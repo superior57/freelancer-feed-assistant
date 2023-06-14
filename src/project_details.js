a16ad29b09bdfcdecafa5c9098f882266558aca1
Elite <petpanda0057@gmail.com>
2023-06-15 00:01:18 +0300

    feat: add client name

--- src/project_details.js
+++ src/project_details.js
@@ -15,91 +15,98 @@ $(document).ready(function () {
 
     $el.append($image);
 
-    $("#bidButton").on("click", () => {
-      const allPath = document.location.pathname.split("/");
-      if (document.location.pathname.match(/^(\/projects\/)([a-z]){2}/)) {
-        let projectPath = [allPath[2], allPath[3]].join("/");
-        console.log("starting bid...");
-        $("#bidButton").hide();
-        $image.show();
+    const allPath = document.location.pathname.split("/");
+    if (document.location.pathname.match(/^(\/projects\/)([a-z]){2}/)) {
+      let projectPath = [allPath[2], allPath[3]].join("/");
+      console.log("getting project info...");
+      $("#bidButton").hide();
+      $image.show();
 
-        axios
-          .get("https://www.freelancer.com/api/projects/0.1/projects", {
-            params: {
-              limit: 1,
-              attachment_details: true,
-              full_description: true,
-              job_details: true,
-              location_details: true,
-              nda_details: true,
-              project_collaboration_details: true,
-              seo_urls: [projectPath],
-              selected_bids: true,
-              qualification_details: true,
-              upgrade_details: true,
-              review_availability_details: true,
-              local_details: true,
-              equipment_details: true,
-              invited_freelancer_details: true,
-              client_engagement_details: true,
-              contract_signature_details: true,
-              enterprise_linked_projects_details: true,
-              equipment_group_details: true,
-              webapp: 1,
-              compact: true,
-              new_errors: true,
-              new_pools: true,
-            },
-          })
-          .then(async (response) => {
-            const details = response.data?.result?.projects?.[0];
+      axios
+        .get("https://www.freelancer.com/api/projects/0.1/projects", {
+          params: {
+            limit: 1,
+            attachment_details: true,
+            full_description: true,
+            job_details: true,
+            location_details: true,
+            nda_details: true,
+            project_collaboration_details: true,
+            seo_urls: [projectPath],
+            selected_bids: true,
+            qualification_details: true,
+            upgrade_details: true,
+            review_availability_details: true,
+            local_details: true,
+            equipment_details: true,
+            invited_freelancer_details: true,
+            client_engagement_details: true,
+            contract_signature_details: true,
+            enterprise_linked_projects_details: true,
+            equipment_group_details: true,
+            webapp: 1,
+            compact: true,
+            new_errors: true,
+            new_pools: true,
+          },
+        })
+        .then(async (response) => {
+          const details = response.data?.result?.projects?.[0];
 
-            const owner_info = await axios
-              .get("https://www.freelancer.com/api/users/0.1/users", {
-                params: {
-                  badge_details: true,
-                  country_details: true,
-                  display_info: true,
-                  employer_reputation: true,
-                  jobs: true,
-                  location_details: true,
-                  membership_details: true,
-                  preferred_details: true,
-                  qualification_details: true,
-                  responsiveness: true,
-                  reputation: true,
-                  sanction_details: true,
-                  status: true,
-                  users: [details.owner_id],
-                  profile_description: true,
-                  marketing_mobile_number: true,
-                  webapp: 1,
-                  compact: true,
-                  new_errors: true,
-                  new_pools: true,
-                },
-              })
-              .then((res) => res.data?.result?.users?.[details.owner_id]);
+          const owner_info = await axios
+            .get("https://www.freelancer.com/api/users/0.1/users", {
+              params: {
+                badge_details: true,
+                country_details: true,
+                display_info: true,
+                employer_reputation: true,
+                jobs: true,
+                location_details: true,
+                membership_details: true,
+                preferred_details: true,
+                qualification_details: true,
+                responsiveness: true,
+                reputation: true,
+                sanction_details: true,
+                status: true,
+                users: [details.owner_id],
+                profile_description: true,
+                marketing_mobile_number: true,
+                webapp: 1,
+                compact: true,
+                new_errors: true,
+                new_pools: true,
+              },
+            })
+            .then((res) => res.data?.result?.users?.[details.owner_id]);
 
-            details.owner_info = owner_info;
-            const bid = await createBid(details);
+          details.owner_info = owner_info;
+          const clientName = owner_info.public_name;
 
-            $("#descriptionTextArea").val(bid.message);
-            $("#descriptionTextArea").click();
-            $("#descriptionTextArea").attr(
-              "class",
-              "TextArea ng-tns-c243-47 ng-trigger ng-trigger-shakeAnimation ng-touched ng-dirty ng-valid"
-            );
-
-            $("#bidButton").show();
-            $image.hide();
-          })
-          .catch((error) => {
-            $("#bidButton").show();
-            $image.hide();
-            console.log(error);
+          $Async("app-employer-info fl-card-header-title", function ($el) {
+            $el.text(`${$el.text()}: ${clientName}`);
           });
-      }
-    });
+
+          $("#bidButton").on("click", () => {
+            createBid(details)
+              .then(() => {
+                $("#descriptionTextArea").val(bid.message);
+                $("#descriptionTextArea").click();
+                $("#descriptionTextArea").attr(
+                  "class",
+                  "TextArea ng-tns-c243-47 ng-trigger ng-trigger-shakeAnimation ng-touched ng-dirty ng-valid"
+                );
+
+                $("#bidButton").show();
+                $image.hide();
+              })
+              .catch((error) => {
+                $("#bidButton").show();
+                $image.hide();
+                console.log(error);
+              });
+          });
+        });
+    }
   });
 });
